2906ff0b62d8b814dffe551e873526fee80f6de8
danielstahl <oe.daniel.stahl@gmail.com>
2016-06-04 12:03:00 +0200

    Added album release date

--- album-info.sh
+++ album-info.sh
@@ -5,4 +5,4 @@ album=$1
 album_id=`cut -d: -f3 <<<"${album}"`
 market=$2
 curlresult=`curl -X GET "https://api.spotify.com/v1/albums/$album_id?market=$market" -H "Accept: application/json" -H "Authorization: Bearer $oauth"`
-echo "$curlresult" | jq '{"uri" : .uri, "name" : .name, "playable" : .tracks.items | unique_by(.is_playable)[].is_playable}'
+echo "$curlresult" | jq '{"uri" : .uri, "name" : .name, "releaseDate": .release_date, "playable" : .tracks.items | unique_by(.is_playable)[].is_playable}'
