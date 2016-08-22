c52b0f8b62048fc826ac0fc515270e77a11be8df
Daniel Ståhl <stahl@spotify.com>
2016-08-22 15:04:50 +0200

    Add shortcut script for albums and tracks

---
+++ markets-album-info.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+album=$1
+
+sh album-info.sh ${album} US
+sh album-info.sh ${album} FR
+sh album-info.sh ${album} DE
+sh album-info.sh ${album} GB 
---
+++ markets-track-info.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+album=$1
+
+sh track-info.sh ${album} US
+sh track-info.sh ${album} FR
+sh track-info.sh ${album} DE
+sh track-info.sh ${album} GB 
